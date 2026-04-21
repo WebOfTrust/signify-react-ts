7eff6873a8e5a568a433930c7cddb20de0430546
Kent Bull <kent@kentbull.com>
2026-04-21 14:23:22 -0600

    dev live reload
    Signed-off-by: Kent Bull <kent@kentbull.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import wasm from 'vite-plugin-wasm';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), wasm()],
+    server: {
+        host: "127.0.0.1",
+        port: 5173,
+    },
 });
